5ea990a365812ba57530d33f6d3290e326484bfe
HaThuy-git <52903176+HaThuy-git@users.noreply.github.com>
2019-07-31 12:02:25 +0700

    update

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello world</title>
+</head>
+<body>
+     <?php 
+     echo "Xin Chao"
+     
+     <img src="Test/anh1.jpg"></img>
+     ?>
+
+</body>
+</html>
